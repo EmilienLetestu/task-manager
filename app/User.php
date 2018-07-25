8a6dfe05d37cf4c64191de7579045260cce13565
emilien letestu <emilien@pc32.home>
2018-07-25 14:36:18 +0200

    create methods to check user role(s)

--- app/User.php
+++ app/User.php
@@ -35,4 +35,44 @@ class User extends Authenticatable
     {
         return $this->belongsToMany('App\Role', 'user_role', 'user_id', 'role_id');
     }
+
+    /**
+     * @param $roles
+     * @return bool
+     */
+    public function hasAnyRole($roles): bool
+    {
+        if(is_array($roles)){
+            foreach ($roles as $role){
+
+                if($this->hasRole($role)){
+
+                    return true;
+                }
+            }
+        } else {
+
+            if($this->hasRole($roles)){
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * @param string $role
+     * @return bool
+     */
+    public function hasRole(string $role): bool
+    {
+        if($this->roles()->where('name', $role)->first()) {
+
+            return true;
+        }
+
+        return false;
+    }
 }
