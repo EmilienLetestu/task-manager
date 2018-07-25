32e8c02ef86065688887890a1c0f15ba2c456dfa
emilien letestu <emilien@pc32.home>
2018-07-25 21:38:32 +0200

    add routes for "task" crud

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,56 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/tasks', [
+    'uses'       => 'TaskController@index',
+    'middleware' => 'roles',
+    'roles'      => ['User','Manager','Admin']
+])
+->name('tasks.index');
+
+Route::get('/tasks/create', [
+    'uses'       => 'TaskController@create',
+    'middleware' => 'roles',
+    'roles'      => ['Manager', 'Admin']
+])
+->name('tasks.create');
+
+Route::post('/tasks', [
+    'uses'       => 'TaskController@store',
+    'middleware' => 'roles',
+    'roles'      => ['Manager', 'Admin']
+])
+->name('tasks.store');
+
+Route::get('/tasks/{task}', [
+    'uses'       => 'TaskController@show',
+    'middleware' => 'roles',
+    'roles'      => ['User', 'Manager', 'Admin']
+])
+->name('tasks.show');
+
+Route::get('/tasks/{task}/edit', [
+    'uses'       => 'TaskController@edit',
+    'middleware' => 'roles',
+    'roles'      => ['Manager', 'Admin']
+])
+->name('tasks.edit');
+
+Route::put('/tasks/{task}', [
+    'uses'       => 'TaskController@update',
+    'middleware' => 'roles',
+    'roles'      => ['Manager', 'Admin']
+]);
+
+Route::delete('/tasks/{task}', [
+    'uses'       => 'TaskController@destroy',
+    'middleware' => 'roles',
+    'roles'      => ['Manager', 'Admin']
+])
+->name('tasks.destroy');
