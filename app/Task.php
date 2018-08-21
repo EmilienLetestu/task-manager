ac1a9ae2d0f50af1d4871f46a35a813e8ac39da3
emilien letestu <emilien@pc32.home>
2018-08-21 11:05:21 +0200

    [model|tasks] validation rules

--- app/Task.php
+++ app/Task.php
@@ -6,12 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    /**
+     * @var array
+     */
     protected $fillable = [
+
         'title', 'description', 'dead_line', 'done'
     ];
 
+    /**
+     * @var array
+     */
     protected $defaults = [
+
         'done' => false
+    ];
+
+    /**
+     * validation rules
+     * @var array
+     */
+    public static $rules = [
+
+        'title'       => 'required|min:4|max:40',
+        'description' => 'required|min:10',
+        'dead_line'   => 'required'
+
     ];
 
     /**
